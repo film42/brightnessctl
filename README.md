b63a24909fff3232646b4698eb698795a01e985e
Garrett Thornburg <film42@gmail.com>
2016-12-11 18:40:43 -0500

    Add simple brightness controller

---
+++ README.md
@@ -0,0 +1,20 @@
+brightnessctl
+=============
+
+A simple brightness controller. It helps change device values like keyboards and displays. You point to the directory
+with a `max_brightness` and `brightness` interface, and specify an `amount`. Everything else is :ok_hand:.
+
+```
+$ ./brightnessctl --help
+Usage of ./brightnessctl:
+  -amount int
+        Increate or decreate by this percentage. Ex: 5 or -5. (default 5)
+  -device string
+  The options are 'keyboard' or 'display' or '/sys/class/etcetcetc'.
+```
+
+# Building
+
+```
+$ go build brightness.go
+```
---
+++ brightnessctl.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"math"
+	"fmt"
+)
+
+func readInt(device string) int {
+	_, err := os.Stat(device)
+	if err != nil {
+		fmt.Println("Error while getting stats of", device, " - ", device)
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadFile(device)
+	if err != nil {
+		fmt.Println("Error while reading", device, " - ", device)
+		os.Exit(1)
+	}
+
+	value, err := strconv.Atoi(strings.Trim(string(data), "\n"))
+	if err != nil {
+		fmt.Println("Error parsing value for", device, " - ", err)
+		os.Exit(1)
+	}
+
+	return value
+}
+
+func writeInt(device string, n int) {
+	data := []byte(strconv.Itoa(n) + "\n")
+	err := ioutil.WriteFile(device, data, os.ModeCharDevice)
+	if err != nil {
+		fmt.Println("Error while writing", n, "to", device, " - ", err)
+		os.Exit(1)
+	}
+}
+
+func changeDeviceByN(device string, n int) {
+	currentBrightness := readInt(device + "/brightness")
+	maxBrightness := readInt(device + "/max_brightness")
+
+	if maxBrightness <= 0 {
+		fmt.Println("Found max_brightness with value=0. This doesn't seem correct.")
+		os.Exit(1)
+	}
+
+	changeBy := (float64(n) / float64(100)) * float64(maxBrightness)
+	newBrightness := float64(currentBrightness) + changeBy
+	newBrightness = math.Max(newBrightness, 0)
+	newBrightness = math.Min(newBrightness, float64(maxBrightness))
+
+	writeInt(device + "/brightness", int(newBrightness))
+}
+
+func main() {
+	devicePtr := flag.String("device", "", "The options are 'keyboard' or 'display' or '/sys/class/etcetcetc'.")
+	amountPtr := flag.Int("amount", 5, "Increate or decreate by this percentage. Ex: 5 or -5.")
+	flag.Parse()
+
+	switch(*devicePtr) {
+	case "":
+		fmt.Println("No device specified:")
+		flag.PrintDefaults()
+	case "keyboard":
+		changeDeviceByN("/sys/class/leds/smc::kbd_backlight/", *amountPtr)
+	case "display":
+		changeDeviceByN("/sys/class/backlight/gmux_backlight/", *amountPtr)
+	default:
+		fmt.Println("Unknown device:", *devicePtr)
+		os.Exit(1)
+	}
+}
